422c9868117890bc2f3c277dfe199a1caeaef026
Loveday <tloveday@lgsdirect.com>
2020-07-12 17:36:23 -0500

    Changed things around

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
+import tkinter as tk
 from tkinter import *
 
-root=Tk()
-import tkinter as tk
-    
+root = Tk()
 
-def write_slogan():
-    print("Tkinter is easy to use!")
 frame = Frame(root)
 frame.pack()
 
-button = Button(frame, 
-                   text="QUIT", 
-                   fg="red",
-                   command=quit)
-button.pack(side=LEFT)
-slogan = Button(frame,
-                   text="Hello",
-                   command=write_slogan)
-slogan.pack(side=LEFT)
+
 
 def twitterClick():
-    myLabel=Label(root,text="Twitter button pressed")
-    myLabel.pack()
-def webClick():
-    myLabel=Label(root,text="Web button pressed")
+    myLabel = Label(root, text="Twitter button pressed")
     myLabel.pack()
 
-newLabel=Label(root,text="OSINT PROJECT")
+
+def youtubeClick():
+    myLabel = Label(root, text="Web button pressed")
+    myLabel.pack()
+
+
+def analyzeClick():
+    myLabel = Label(root, text="Time to analyze the code")
+    myLabel.pack()
+
+
+newLabel = Label(root, text="OSINT PROJECT")
 newLabel.pack()
-twitButton=Button(root,text="Twitter", command=twitterClick)
+twitButton = Button(root, text="Twitter", command=twitterClick)
 twitButton.pack()
-webButton=Button(root,text="Grab web data", command=webClick)
+webButton = Button(root, text="Grab Youtube Data", command=youtubeClick)
 webButton.pack()
+analysisButton = Button(root, text="Analyze Data", command=analyzeClick)
+analysisButton.pack()
+button = Button(frame,text="QUIT",fg="red",command=quit)
+button.pack()
 
 root.mainloop()
