f86046f825435781105043f282690516841bd655
Timothy Truty <ttruty@gmail.com>
2020-07-16 20:23:37 -0500

    output to output folder and renames based on search query

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from nltk.tokenize import word_tokenize
 import nlp_test
@@ -9,10 +10,17 @@ def get_sentiment(row):
     sentiment = nlp_test.classifier.classify(dict([token, True] for token in custom_tokens))
     return sentiment
 
-def analyze_tweets():
+def analyze_tweets(query):
+    output_dir = "twitter_output"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
+    input_csv = f"{query}_twitter_output.csv"
+    input_csv = os.path.join(output_dir, input_csv)
+
+    # read csv in
     df = pd.read_csv(
-        "twitter_output.csv"
+        input_csv
     )
 
     # df cleaning , remove blank text and Nan values
@@ -22,16 +30,21 @@ def analyze_tweets():
 
     # add sentament column
     df["sentiment"] = df.apply(lambda row: get_sentiment(row), axis=1)
-    #
-    df.to_csv('twitter_with_sentiment.csv', index=False)
 
-    #username_series = df.groupby("username")["sentiment"].value_counts(normalize=True).mul(100)
+    # output the sentament to a csv
+    out_sentiment_file = f"{query}_twitter_with_sentiment.csv"
+    out_sentiment_file = os.path.join(output_dir, out_sentiment_file)
+    df.to_csv(out_sentiment_file, index=False)
+
+    # get the percentage of positive vs negative and save in a csv
     username_series = df.groupby("username")["sentiment"].value_counts(normalize=True).mul(100).to_frame()
-    username_series.to_csv('tweet_sentiment_overview.csv')
 
+    out_sentiment__overview_file = f"{query}_sentiment_overview.csv"
+    out_sentiment__overview_file = os.path.join(output_dir, out_sentiment__overview_file)
+    username_series.to_csv(out_sentiment__overview_file)
 
-    username_df =  df["username"].value_counts()
-    return username_df.head(10)
-
-    #nlp_test.classifier.classify(dict([token, True] for token in nlp_test.remove_noise(word_tokenize(df["text"]))
+    # Dataframes to print to screem
+    overall_sentiment = df["sentiment"].value_counts()
+    username_df = df["username"].value_counts()
+    return username_df.head(10), overall_sentiment
 
