f86c73bb4cac194e368a76f9391bb9c59bd49b9c
Trevor Loveday <trevorloveday22@gmail.com>
2020-07-18 20:09:16 -0500

    Both  threads running smoothly.  Youtube pulling and creating csv to analyze

--- dependency.sh
+++ dependency.sh
@@ -1,9 +1,6 @@
-apt install -y python3 python3-tk python3-bs4 python3-pip
-pip3 install requests
-pip3 install lxml
-pip3 install pyquery
-pip3 install socialreaper
-pip3 install nltk
-pip3 install GetOldTweets3
+apt install -y python3 python-bs4 python3-tk python3-bs4 python3-pip
+pip3 install requests nltk lxml pyquery socialreaper numpy GetOldTweets3 oauthlib PyQt5 QDarkStyle sip urllib3 appdirs idna chardet
+
 python3 -m pip install pandas
 python3 nltk_helper.py
+
